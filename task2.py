99182f640f7f409196274943c682691113c5a2f6
android156 <n_degtyarev@mail.ru>
2021-12-23 15:51:37 +0300

    task2 done

--- task2.py
+++ task2.py
@@ -1,3 +1,7 @@
 # Каждое из слов «class», «function», «method» записать в байтовом типе без преобразования в последовательность
 # кодов (не используя методы encode и decode) и определить тип, содержимое и длину соответствующих переменных.
 
+word_list = ['class', 'function', 'method']
+b_word_list = [b'class', b'function', b'method']
+for b_word in b_word_list:
+    print(f'{b_word} - тип {type(b_word)}, длина {len(b_word)}')
